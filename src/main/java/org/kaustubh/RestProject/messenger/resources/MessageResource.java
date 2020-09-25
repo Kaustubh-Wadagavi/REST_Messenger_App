5d2806630caf8ce0aa927551632d14552e19937a
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2020-09-25 18:35:45 +0530

    Added response status codes, and sending resource URI of messanger app

--- src/main/java/org/kaustubh/RestProject/messenger/resources/MessageResource.java
+++ src/main/java/org/kaustubh/RestProject/messenger/resources/MessageResource.java
@@ -1,5 +1,6 @@
 package org.kaustubh.RestProject.messenger.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import javax.ws.rs.BeanParam;
@@ -11,7 +12,10 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 import org.kaustubh.RestProject.messenger.model.Message;
 import org.kaustubh.RestProject.messenger.resources.beans.MessageFilterBean;
@@ -32,12 +36,19 @@ public class MessageResource {
 		if(filterBean.getStart() >= 0 && filterBean.getSize() >= 0) {
 			return messageService.getAllMessagesPaginated(filterBean.getStart(), filterBean.getSize());
 		}
- 		return messageService.getAllMessages();
+		return messageService.getAllMessages();
 	}
 
 	@POST
-	public Message addMessage(Message message) {
-		return messageService.addMessage(message);
+	public Response addMessage(Message message, @Context UriInfo uriInfo) {
+
+		Message newMessage = messageService.addMessage(message);
+		String newId = String.valueOf(newMessage.getId());
+		URI uri = uriInfo.getBaseUriBuilder().path(newId).build();
+		return Response.created(uri)
+				.entity(newMessage)
+				.build();
+		//return messageService.addMessage(message);
 	}
 
 	@PUT
@@ -58,7 +69,7 @@ public class MessageResource {
 	public Message getMessage(@PathParam("messageId")long id) {
 		return messageService.getMessage(id);	
 	}
-	
+
 	@Path("/{messageId}/comments")
 	public CommentResource getCommentResource() {
 		return new CommentResource();
